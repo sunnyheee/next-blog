16c789bb0ea68c04ce3352d57bb8091d1058c1b5
sunnyheee <fool0116@naver.com>
2023-10-28 11:21:56 +0900

    article id fetch

---
+++ src/app/articles/[id]/page.tsx
@@ -0,0 +1,31 @@
+import { getDetailArticle } from "@/blogAPI";
+
+import Image from "next/image";
+
+import React from "react";
+
+const Article = async ({ params }: { params: { id: string } }) => {
+  const detailArticle = await getDetailArticle(params?.id);
+  console.log(params?.id);
+
+  return (
+    <div className="max-w-3xl mx-auto p-5">
+      <Image
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
+        width={1280}
+        height={300}
+        alt=""
+      />
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
+      <div>
+        <p className="text-lg leading-relaxed text-justify">
+          {detailArticle.content}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticle = async (): Promise<Article[]> => {
@@ -11,4 +12,25 @@ export const getAllArticle = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    // cache: "no-store",
+    next: { revalidate: 60 },
+  }); //ISR
+
+  if (res.status === 404) {
+    notFound();
+    throw new Error("게시물을 찾을 수 없습니다");
+  }
+
+  if (!res.ok) {
+    throw new Error("This is Error");
+  }
+
+  await new Promise((reslove) => setTimeout(reslove, 1000));
+
+  const article = await res.json();
+  return article;
 };
